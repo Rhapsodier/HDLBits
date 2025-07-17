956ba0c78ad1db8bdb727a3b0805f9a24242729e
Rhapsodier <2724025086@qq.com>
2025-07-17 16:53:30 +0800

    完成了More Verilog Feature 这个模块

---
+++ More_Verilog_Features/Bcdadd_100.sv
@@ -0,0 +1,32 @@
+/*
+You are provided with a BCD one-digit adder named bcd_fadd that adds two BCD digits and carry-in, and produces a sum and carry-out.
+
+module bcd_fadd (
+    input [3:0] a,
+    input [3:0] b,
+    input     cin,
+    output   cout,
+    output [3:0] sum );
+Instantiate 100 copies of bcd_fadd to create a 100-digit BCD ripple-carry adder. Your adder should add two 100-digit BCD numbers (packed into 400-bit vectors) and a carry-in to produce a 100-digit sum and carry out.
+*/
+module top_module( 
+    input [399:0] a, b,
+    input cin,
+    output cout,
+    output [399:0] sum );
+   
+   
+    wire[100:0] carry;
+    genvar i;
+   assign carry[0] = cin;
+    assign cout = carry[100];
+   generate
+    for ( i=0 ; i<100 ; i++) begin : ADD
+        bcd_fadd fa(
+            a[i*4 +: 4], b[i*4 +: 4], carry[i], carry[i+1], sum[i*4 +: 4]
+        );
+    end
+   endgenerate 
+endmodule
+
+
